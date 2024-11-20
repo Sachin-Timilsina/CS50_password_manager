be0a1fa7ecc42ea5431be07cbda82661adc7a4fd
Sachin Timilsina <sachintimilsina11@gmail.com>
2024-11-20 15:46:32 +0545

    added login and signup pages

--- app/app.py
+++ app/app.py
@@ -1,4 +1,18 @@
-from app import app
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,1 +1,29 @@
-console.log("Password Manager JS Loaded");
+// Toggle visibility of confirm password
+function toggleConfirmPassword() {
+    var confirmPasswordField = document.getElementById("confirm_password");
+    var confirmEyeIcon = document.getElementById("confirm-eye-icon");
+    if (confirmPasswordField.type === "password") {
+        confirmPasswordField.type = "text";
+        confirmEyeIcon.classList.remove("bi-eye");
+        confirmEyeIcon.classList.add("bi-eye-slash");
+    } else {
+        confirmPasswordField.type = "password";
+        confirmEyeIcon.classList.remove("bi-eye-slash");
+        confirmEyeIcon.classList.add("bi-eye");
+    }
+}
+
+// Toggle visibility of password
+function togglePassword() {
+    var passwordField = document.getElementById("password");
+    var eyeIcon = document.getElementById("eye-icon");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeIcon.classList.remove("bi-eye");
+        eyeIcon.classList.add("bi-eye-slash");
+    } else {
+        passwordField.type = "password";
+        eyeIcon.classList.remove("bi-eye-slash");
+        eyeIcon.classList.add("bi-eye");
+    }
+}
